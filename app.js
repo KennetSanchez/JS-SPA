82f8816480ad2e64b3c3630e5a09b556b3de9510
KennetSanchez <kesasaro@gmail.com>
2022-02-12 19:48:15 -0500

    Router functionality finished

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ const router = async () => {
 window.addEventListener("hashchange", router);
 
 window.addEventListener("load", router);
+
+
+parseRequestURL: () => {
+    let url = location.hash.slice(1).toLowerCase() || "/";
+    let r = url.split("/");
+    let request = {
+      resource: null,
+      id: null,
+      verb: null,
+    };
+    request.resource = r[1];
+    request.id = r[2];
+    request.verb = r[3];
+  
+    return request;
+  };
+  

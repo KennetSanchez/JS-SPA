78d0ad08ffbdf29d527ecf4ca25bb9dacd872628
KennetSanchez <kesasaro@gmail.com>
2022-02-13 00:08:09 -0500

    Footer section added
    The footer section has been added.

--- app.js
+++ app.js
@@ -58,3 +58,22 @@ let About = {
 }
 
 export default About;
+
+let Bottombar = {
+  render: async () => {
+      let view = `
+      <footer class="footer">
+          <div class="content has-text-centered">
+              <p>
+                  This is my foot. There are many like it, but this one is(not) mine.
+              </p>
+          </div>
+      </footer>
+      `
+      return view
+  },
+  after_render: async () => { }
+
+}
+
+export default Bottombar;
